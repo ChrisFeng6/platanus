c9f16878107d8101e1d1a71f29033678ce981c3b
ChrisFeng <78003571+ChrisFeng6@users.noreply.github.com>
2024-02-05 22:47:36 +0800

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -25,22 +25,29 @@ let firstPeople = [];
 
 let backgroundImg;
 
+// distance contants
+let canvasWidth = 1456;
+let canvasHeight = 816;
+
 
 
 function setup() {
   // this has to match the size of the background image
-  createCanvas(1456, 816); 
+  createCanvas(canvasWidth, canvasHeight); 
   frameRate(30);
   //draw button
+  let buttonWidth = 190
   let col = color(234, 204, 144, 100);
   let whiteCol = color(255,255,255);
+  let transparentCol = color(255,255,255,0)
   Button1 = createButton("Draw Fourth Prize");
-  Button1.style('background-color', col);
-  Button1.position(40, 210);
-  Button1.style('border', whiteCol);
-  Button1.style('border-radius', 50);
-  Button1.style('font-size', 50);
-  Button1.size(140,40)
+  Button1.style('background-color', transparentCol);
+  Button1.position(canvasWidth/2-buttonWidth/2, 185);
+  Button1.style('border', '2px solid white');
+  Button1.style('font-size', '20px');
+  Button1.style('color', 'white');
+  Button1.style('border-radius', '10px');
+  Button1.size(buttonWidth,45)
   Button1.mousePressed(pickOne);
   
   
@@ -76,6 +83,7 @@ function setup() {
 function preload(){
   backgroundImg = loadImage('assets/bg_1x.png'); 
 }
+
 
 
 function pickOne() {
@@ -179,37 +187,88 @@ function draw() {
 }
 
 function drawText(){
+  let upperMargin = 351;
+  let lineHeight = 40;
+  let paragraphHeight = 75;
+  let centerX = canvasWidth/2; // this is the x distance of the center line
+  let prizeTextSize = 28;
+  let peopleTextSize = 20;
+  let lineLength = 266
+  let lineShapeHeight = 8
+  textSize(20);
+  textAlign(CENTER);
+  textFont('Times New Roman');
+  textStyle(BOLD);
+  fill('white');
+
+  // draw prize text
+  textSize(prizeTextSize);
+  text('First Prize $1000', centerX, upperMargin);
+  text('Second Prize: $500', centerX, upperMargin+paragraphHeight + lineHeight*1);
+  text('Third Prize: $200', centerX, upperMargin+paragraphHeight*2 + lineHeight*2);
+  text('Fourth Prize: $100', centerX, upperMargin+paragraphHeight*3 + lineHeight*3);
+
+  // draw people text
+  textSize(peopleTextSize);
+  textStyle(BOLD);
+  text(firstPeople, centerX, upperMargin+lineHeight*1)
+  textStyle(NORMAL);
+  text(secondPeople, centerX, upperMargin+paragraphHeight + lineHeight*2)
+  text(thirdPeople, centerX, upperMargin+paragraphHeight*2 + lineHeight*3)
+  text(fourthPeople, centerX, upperMargin+paragraphHeight*3 + lineHeight*4)
+
+  //draw line
+  stroke(255);
+  strokeWeight(1.5);
+  let lineLeft = centerX-lineLength/2; 
+  let lineRight = centerX+lineLength/2;
+  line(lineLeft, upperMargin+lineShapeHeight, lineRight, upperMargin+lineShapeHeight);
+  line(lineLeft, upperMargin+lineShapeHeight+paragraphHeight+lineHeight, lineRight, upperMargin+lineShapeHeight+paragraphHeight+lineHeight);
+  line(lineLeft, upperMargin+lineShapeHeight+paragraphHeight*2+lineHeight*2, lineRight, upperMargin+lineShapeHeight+paragraphHeight*2+lineHeight*2);
+  line(lineLeft, upperMargin+lineShapeHeight+paragraphHeight*3+lineHeight*3, lineRight, upperMargin+lineShapeHeight+paragraphHeight*3+lineHeight*3);
+  strokeWeight(0);
+}
+
+// the old draw text function from 2023
+/*
+function drawText(){
   let upperMargin = 300;
   let lineHeight = 25;
-  let paragraphHeight = 50
+  let paragraphHeight = 50;
+  let centerX = canvasWidth/2; // this is the x distance of the center line
   textSize(20);
+  textAlign(CENTER);
   fill('white');
-  text('First Prize: USD 1000', 40, upperMargin);
-  text('Number of Prizes: 1', 40, upperMargin+lineHeight);
-  text('Winner: ', 40, upperMargin+lineHeight*2)
+  text('First Prize $1000', centerX, upperMargin);
+  text('Number of Prizes: 1', centerX, upperMargin+lineHeight);
+  text('Winner: ', centerX, upperMargin+lineHeight*2)
   text(firstPeople, 110, upperMargin+lineHeight*2)
   
-  text('Second Prize: USD 500', 40, upperMargin+paragraphHeight + lineHeight*2);
-  text('Number of Prizes: 4', 40, upperMargin+paragraphHeight + lineHeight*3);
-  text('Winner: ', 40, upperMargin+paragraphHeight + lineHeight*4)
+  text('Second Prize: $500', centerX, upperMargin+paragraphHeight + lineHeight*2);
+  text('Number of Prizes: 4', centerX, upperMargin+paragraphHeight + lineHeight*3);
+  text('Winner: ', centerX, upperMargin+paragraphHeight + lineHeight*4)
   text(secondPeople, 110, upperMargin+paragraphHeight + lineHeight*4)
   
-  text('Third Prize: USD 200', 40, upperMargin+paragraphHeight*2 + lineHeight*4);
-  text('Number of Prizes: 8', 40, upperMargin+paragraphHeight*2 + lineHeight*5);
-  text('Winner: ', 40, upperMargin+paragraphHeight*2 + lineHeight*6)
+  text('Third Prize: $200', centerX, upperMargin+paragraphHeight*2 + lineHeight*4);
+  text('Number of Prizes: 8', centerX, upperMargin+paragraphHeight*2 + lineHeight*5);
+  text('Winner: ', centerX, upperMargin+paragraphHeight*2 + lineHeight*6)
   text(thirdPeople, 110, upperMargin+paragraphHeight*2 + lineHeight*6)
   
-  text('Fourth Prize: USD 100', 40, upperMargin+paragraphHeight*3 + lineHeight*6);
-  text('Number of Prizes: 8', 40, upperMargin+paragraphHeight*3 + lineHeight*7);
-  text('Winner: ', 40, upperMargin+paragraphHeight*3 + lineHeight*8)
+  text('Fourth Prize: $100', centerX, upperMargin+paragraphHeight*3 + lineHeight*6);
+  text('Number of Prizes: 8', centerX, upperMargin+paragraphHeight*3 + lineHeight*7);
+  text('Winner: ', centerX, upperMargin+paragraphHeight*3 + lineHeight*8)
   text(fourthPeople, 110, upperMargin+paragraphHeight*3 + lineHeight*8)
 }
+*/
+
 
 function drawFlashing(){
   let peopleLeft = nameList.length;
   let num = getRndInteger(0, peopleLeft)
   let nameDisplayed = nameList[num]
   textSize(60);
+  textStyle(BOLD);
+  textFont('Times New Roman');
   if (state == "fourthCompleted") {
     nameDisplayed = fourthPeople[7];
   }
@@ -222,7 +281,7 @@ function drawFlashing(){
   if (state == "firstCompleted") {
     nameDisplayed = firstPeople[0];
   }
-  text(nameDisplayed, 20, 120);
+  text(nameDisplayed, canvasWidth/2-15, 135);
 }
 
 function getRndInteger(min, max) {
